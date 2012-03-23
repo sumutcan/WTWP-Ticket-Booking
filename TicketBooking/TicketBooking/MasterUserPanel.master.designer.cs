d00fe24b6f75e1a4f910fb278c44ab3472cec89c
Yigitcan SENER <ycansener@gmail.com>
2012-03-23 14:57:46 +0200

    adRotator Eklendi.

--- TicketBooking/TicketBooking/MasterUserPanel.master.designer.cs
+++ TicketBooking/TicketBooking/MasterUserPanel.master.designer.cs
@@ -192,6 +192,24 @@ public partial class MasterUserPanel {
     protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     
     /// <summary>
+    /// AdRotator1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.AdRotator AdRotator1;
+    
+    /// <summary>
+    /// XmlDataSource1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource1;
+    
+    /// <summary>
     /// loginControl1 control.
     /// </summary>
     /// <remarks>
